e9c24e715b4cb30d436ecad586a69a94d578ac8f
cccanb <cccanb@gmail.com>
2026-03-28 10:33:00 +0100

    refactor: update send method to use WebhookNotification type and add production docker-compose file

--- app/channels/webhook.py
+++ app/channels/webhook.py
@@ -1,5 +1,4 @@
 import logging
-from typing import Any
 from app.channels import BaseChannel
 from app.models import WebhookNotification
 
@@ -9,5 +8,5 @@ logger = logging.getLogger(__name__)
 class WebhookChannel(BaseChannel):
     schema = WebhookNotification
 
-    def send(self, event: dict[str, Any]) -> None:
+    def send(self, event: WebhookNotification) -> None:
         ...
